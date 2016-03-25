af9c8c78f0be7279a44a287f025452da86026614
Nick Kardakov <nikita.kardakov@shazam.com>
2016-03-25 11:53:13 +0000

    Project

---
+++ Midiplayer/Midiplayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Midiplayer
+//
+//  Created by Nikita.Kardakov on 25/03/2016.
+//  Copyright © 2016 nickk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
